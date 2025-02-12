0916e8c5a945811cdd1d5902ad80c6879d67e4ad
vsb <vsb@cin.ufpe.br>
2025-02-12 20:29:04 -0300

    chore: removing unnecessary code

--- src/main/java/com/example/taskManager/service/TaskService.java
+++ src/main/java/com/example/taskManager/service/TaskService.java
@@ -1,16 +1,10 @@
 package com.example.taskManager.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.taskManager.model.Task;
 import com.example.taskManager.repository.TaskRepository;
-
-import java.util.Date;
-import java.util.TimeZone;
-import java.util.Calendar;
 
 @Service
 public class TaskService extends BaseServiceImpl<Task> {
@@ -20,14 +14,6 @@ public class TaskService extends BaseServiceImpl<Task> {
     public TaskService(TaskRepository taskRepository) {
         super(taskRepository);
         this.taskRepository = taskRepository;
-    }
-
-    public Iterable<Task> findAll() {
-        return taskRepository.findAll();
-    }
-
-    public Optional<Task> findById(String id) {
-        return taskRepository.findById(id);
     }
 
     public Task save(Task entity) {
